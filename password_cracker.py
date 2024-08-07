58ddec3977644a02f79165c67ff4e86aa1945888
creinis <115181527+creinis@users.noreply.github.com>
2024-08-07 15:23:24 +0200

    feat: The function should take an optional second argument named use_salts. If set to true, each salt string from the file known-salts.txt should be appended AND prepended to each password from top-10000-passwords.txt before hashing and before comparing it to the hash passed into the function.

--- password_cracker.py
+++ password_cracker.py
@@ -5,5 +5,10 @@ def crack_sha1_hash(hash, use_salts=False):
     with open('top-10000-passwords.txt', 'r') as f:
         passwords = f.read().splitlines()
     
+    if use_salts:
+        with open('known-salts.txt', 'r') as f:
+            salts = f.read().splitlines()
+    else:
+        salts = ['']
     
     return "PASSWORD NOT IN DATABASE"
