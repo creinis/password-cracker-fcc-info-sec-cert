2399ac7cdb6dca03bd3e4362190e24c3c81fb0bf
creinis <115181527+creinis@users.noreply.github.com>
2024-08-07 15:25:05 +0200

    feat: appended AND prepended every password and salt

--- password_cracker.py
+++ password_cracker.py
@@ -11,4 +11,13 @@ def crack_sha1_hash(hash, use_salts=False):
     else:
         salts = ['']
     
+    for password in passwords:
+        for salt in salts:
+            if hashlib.sha1((salt + password).encode('utf-8')).hexdigest() == hash:
+                return password
+            if hashlib.sha1((password + salt).encode('utf-8')).hexdigest() == hash:
+                return password
+            if hashlib.sha1((salt + password + salt).encode('utf-8')).hexdigest() == hash:
+                return password
+    
     return "PASSWORD NOT IN DATABASE"
