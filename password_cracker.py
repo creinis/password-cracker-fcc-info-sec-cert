54750845b9a18356b45d6b77c1c88532d92b296e
creinis <115181527+creinis@users.noreply.github.com>
2024-08-07 15:22:24 +0200

    feat: The function should hash each password from top-10000-passwords.txt and compare it to the hash passed into the function.

--- password_cracker.py
+++ password_cracker.py
@@ -2,4 +2,8 @@ import hashlib
 
 def crack_sha1_hash(hash, use_salts=False):
     
+    with open('top-10000-passwords.txt', 'r') as f:
+        passwords = f.read().splitlines()
+    
+    
     return "PASSWORD NOT IN DATABASE"
