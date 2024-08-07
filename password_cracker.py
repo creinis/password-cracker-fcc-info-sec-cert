f3079afd7cdf3b26a0e163ea275b5bf12be076aa
creinis <115181527+creinis@users.noreply.github.com>
2024-08-07 15:27:37 +0200

    test: passed all tests

--- password_cracker.py
+++ password_cracker.py
@@ -21,3 +21,11 @@ def crack_sha1_hash(hash, use_salts=False):
                 return password
     
     return "PASSWORD NOT IN DATABASE"
+
+# Testando a função
+print(crack_sha1_hash('b305921a3723cd5d70a375cd21a61e60aabb84ec'))  # Deve retornar 'sammy123'
+print(crack_sha1_hash('c7ab388a5ebefbf4d550652f1eb4d833e5316e3e'))  # Deve retornar 'abacab'
+print(crack_sha1_hash('5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'))  # Deve retornar 'password'
+print(crack_sha1_hash('53d8b3dc9d39f0184144674e310185e41a87ffd5', use_salts=True))  # Deve retornar 'superman'
+print(crack_sha1_hash('da5a4e8cf89539e66097acd2f8af128acae2f8ae', use_salts=True))  # Deve retornar 'q1w2e3r4t5'
+print(crack_sha1_hash('ea3f62d498e3b98557f9f9cd0d905028b3b019e1', use_salts=True))  # Deve retornar 'bubbles1'
